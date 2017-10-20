0f950a9e31b936e6bf77159c58d1e9fa5443ff85
mri3in <i3in.3vil@gmail.com>
2017-10-20 16:46:25 +0700

    update config site name

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'construction');
+define('DB_NAME', 'linh-construction');
 
 /** MySQL database username */
 define('DB_USER', 'root');
